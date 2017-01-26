4dcfab37774f6ddf6f03cbfc8b5f81cd7699a6fc
Ben Dilts <ben@lucidchart.com>
2017-01-26 14:47:57 -0700

    Add support for ts.SymbolFlags.ObjectLiteral

--- src/type-translator.ts
+++ src/type-translator.ts
@@ -362,7 +362,8 @@ export class TypeTranslator {
         return '?';
       }
 
-      if (type.symbol.flags === ts.SymbolFlags.TypeLiteral) {
+      if (type.symbol.flags === ts.SymbolFlags.TypeLiteral ||
+          type.symbol.flags === ts.SymbolFlags.ObjectLiteral) {
         return this.translateTypeLiteral(type);
       } else if (
           type.symbol.flags === ts.SymbolFlags.Function ||
@@ -381,7 +382,6 @@ export class TypeTranslator {
       Tuple
       Mapped
       Instantiated
-      ObjectLiteral
       EvolvingArray
       ObjectLiteralPatternWithComputedProperties
     */
